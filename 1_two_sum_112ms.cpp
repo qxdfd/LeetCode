630f4bc7ef2d5379df4e261979fd3cc48de282ea
qxdfd <919517998@qq.com>
2017-12-11 18:43:19 +0800

    Create 1_two_sum_112ms.cpp

---
+++ 1_two_sum_112ms.cpp
@@ -0,0 +1,56 @@
+//112ms
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int size1 = 0,size2 = 0,size3 = 0;
+        vector<int> pair;
+        vector<vector<int>> num1,num2,num3;
+        vector<int> result;
+        for(int i = 0;i < nums.size();i ++)
+        {
+            if(nums[i] < target/2)
+            {
+                pair.push_back(nums[i]);
+                pair.push_back(i);
+                num1.push_back(pair);
+                pair.erase(pair.begin(),pair.end()); 
+                size1++;
+            }  
+            else if(nums[i] > target/2)
+            {
+                pair.push_back(nums[i]);
+                pair.push_back(i);
+                num2.push_back(pair);
+                pair.erase(pair.begin(),pair.end()); 
+                size2++;
+            } 
+            else
+            {
+                pair.push_back(nums[i]);
+                pair.push_back(i);
+                num3.push_back(pair);
+                pair.erase(pair.begin(),pair.end()); 
+                size3++;
+            }
+        }
+        if(size3 == 2)
+        {
+            result.push_back(num3[0][1]);
+            result.push_back(num3[1][1]);
+            return result;
+        }
+        for(int i = 0;i< size1;i++)
+        {
+            for(int k = 0;k< size2;k++)
+            {
+                if(num1[i][0] + num2[k][0] == target)
+                {
+                    result.push_back(num1[i][1]);
+                    result.push_back(num2[k][1]);
+                    return result;
+                }
+            }
+        }
+
+    }
+};
